b2119f4a54c37d502c18c3f1a17f8b5a333233d9
Sophia Johannsen <sophia_ella_margarethe.johannsen@smail.th-koeln.de>
2021-02-09 16:45:39 +0100

    JSON aus string extrahieren versuch1

--- Datenbank/datenbank.js
+++ Datenbank/datenbank.js
@@ -1,11 +1,29 @@
-const knex = require("./knex.js");
+const knex = require("./knex.js")
+const sqlite3 = require('sqlite3').verbose()
 
 function getMittelpunkt(laendercode) {
     return knex("mittelpunkt").where("laendercode", laendercode).select();
 }
 
 function getGeojson(laendercode) {
-    return knex("geo_daten").where("laendercode", laendercode).select();
+    //return knex("geo_daten").where("laendercode", laendercode).select();
+    knex("geo_daten").where("laendercode", laendercode).select().then(data=>{
+        //console.log(data[0].geo_json)
+
+         //extrahiert das JSON objekt aus dem string    
+        let obj= JSON.parse(data[0].geo_json)
+       
+
+    //ausgabe zur kontrolle
+        console.log(data)
+        //console.log(data.geo_daten)
+
+        //ersetzt den string durch das objekt
+        data.geo_daten=obj
+
+        //gibt datenobjekt zurück (momentan leer)
+        return data
+    })
 }
 
 function getProduce(name) {
--- Datenbank/index.js
+++ Datenbank/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const sqlite3 = require('sqlite3').verbose();
 const db=require('./datenbank.js')
 
 
