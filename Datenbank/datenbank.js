8d8a51ce9a685c884b1e1350be50ffd4c642d42f
Sophia Johannsen <sophia_ella_margarethe.johannsen@smail.th-koeln.de>
2021-02-10 18:40:52 +0100

    datenbank added

---
+++ Datenbank/datenbank.js
@@ -0,0 +1,20 @@
+const knex = require("./knex.js")
+
+function getMittelpunkt(laendercode) {
+    return knex("mittelpunkt").where("laendercode", laendercode).select();
+}
+
+function getGeojson(laendercode) {
+    return knex("geo_daten").where("laendercode", laendercode).select()
+}
+
+
+function getProduce(name) {
+    return knex("produce").where("name", name).select();
+}
+
+module.exports = {
+    getMittelpunkt,
+    getGeojson,
+    getProduce
+};
